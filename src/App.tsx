be19436a88fb774cce2b4940052e74e24d2d38b2
Pattaraporn-sri <pattaraporn.sri@kkumail.com>
2025-01-06 20:15:03 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import "./App.css";
-import axios, { AxiosResponse } from "axios";
-import { DisneyCharacter } from "./Model/ApiModel";
-import Home from "./Components/Home";
+// import axios, { AxiosResponse } from "axios";
 import DemonPage from "./Components/DemonPage";
 
 function App() {
-  const [data, setData] = useState<DisneyCharacter[]>([]);
+  // const [data, setData] = useState<DisneyCharacter[]>([]);
 
-  //--------------Axiso
-  const getDataAsyncByAxios = async () => {
-    const url = "https://api.disneyapi.dev/character";
-    try {
-      const response: AxiosResponse<DisneyCharacter[]> = await axios.get(url);
-      if (response.status === 200) {
-        console.log(response.data);
-        setData(response.data);
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      console.log("finally");
-    }
-  };
+  // //--------------Axiso
+  // const getDataAsyncByAxios = async () => {
+  //   const url = "https://api.disneyapi.dev/character";
+  //   try {
+  //     const response: AxiosResponse<DisneyCharacter[]> = await axios.get(url);
+  //     if (response.status === 200) {
+  //       console.log(response.data);
+  //       setData(response.data);
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   } finally {
+  //     console.log("finally");
+  //   }
+  // };
 
-  useEffect(() => {
-    getDataAsyncByAxios();
-  }, []);
+  // useEffect(() => {
+  //   getDataAsyncByAxios();
+  // }, []);
 
   return (
     <>
